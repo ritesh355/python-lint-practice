b9fcdf45df8d85661f0ed3dbb6361e31943cd421
ritesh355 <riteshsingh86991@gmail.com>
2025-08-05 19:23:10 +0530

    update hello.py

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 def hello():
-  print("Hello World")
-  x = 5
-  print(  "Too many spaces in parentheses"  )
-  print('This is a very long line that will exceed the typical 79 character limit used by most PEP8 linters to test the limits of our linting system and check if we get a warning')
+    print("Hello World")
+    x = 5
+    print("Too many spaces in parentheses")
+    print("This is a long line, but now it's within the limit")
+
